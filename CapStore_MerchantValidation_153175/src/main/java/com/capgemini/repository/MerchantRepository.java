a9b11259eedf4d4d670da9042b8c5174887507e0
HarshiniUppalapati <40890907+HarshiniUppalapati@users.noreply.github.com>
2018-08-21 17:34:48 +0530

    Add files via upload

---
+++ CapStore_MerchantValidation_153175/src/main/java/com/capgemini/repository/MerchantRepository.java
@@ -0,0 +1,17 @@
+package com.capgemini.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.model.Merchant;
+@Repository
+public interface MerchantRepository extends JpaRepository<Merchant, Integer>{
+	
+	@Query(value="select m from Merchant m where m.status=?1")
+	public List<Merchant> getDetailsofMerchant(String status);
+	
+    
+}
